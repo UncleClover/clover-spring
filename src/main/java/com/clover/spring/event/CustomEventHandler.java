01b8d49711f7eb9ed0db8e5738d9db75d8d1f75b
clover <qiang900714@126.com>
2018-01-17 16:02:21 +0800

    spring event

---
+++ src/main/java/com/clover/spring/event/CustomEventHandler.java
@@ -0,0 +1,9 @@
+package com.clover.spring.event;
+
+import org.springframework.context.ApplicationListener;
+
+public class CustomEventHandler implements ApplicationListener<CustomEvent> {
+	public void onApplicationEvent(CustomEvent event) {
+		System.out.println(event.toString());
+	}
+}
--- src/main/test/com/spring/test/VipTest.java
+++ src/main/test/com/spring/test/VipTest.java
@@ -2,23 +2,34 @@ package com.spring.test;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.clover.spring.bean.Vip;
+import com.clover.spring.event.CustomEventPublisher;
 
 public class VipTest {
-	
+
 	@Test
-	public void userNameTest(){
+	public void userNameTest() {
 		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		context.start();
+
 		Vip vip = (Vip) context.getBean("vip");
 		vip.isBlank("clover");
 		System.out.println(vip.getCode());
-		
+
 		System.out.println("=======================================");
 		vip.printMessage();
-		
+
 		vip.sayHi();
+
+		System.out.println("**************************************************");
+		CustomEventPublisher cvp = (CustomEventPublisher) context.getBean("customEventPublisher");
+		cvp.publish();
+		System.out.println("**************************************************");
+
+		context.stop();
 	}
 }
